a98484dc89588bc68a9fb8e49dba4127760ad769
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-05-14 12:24:36 -0400

    prompt is working much better now and renders in html

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,22 @@
-console.log("test")
-//main
 import OpenAI from "openai-api";
 
-// Load your key from an environment variable or secret management service
-// (do not include your key directly in your code)
 const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
-
 const openai = new OpenAI(OPENAI_API_KEY);
 
-(async () => {
-    const gptResponse = await openai.complete({
-        engine: 'davinci',
-        prompt: 'this is a test',
-        maxTokens: 5,
-        temperature: 0.9,
-        topP: 1,
-        presencePenalty: 0,
-        frequencyPenalty: 0,
-        bestOf: 1,
-        n: 1,
-        stream: false,
-        stop: ['\n', "testing"]
-    });
+const main = async () => {
+	const gptResponse = await openai.complete({
+		engine: "text-davinci-002",
+		prompt: "tell me a happy story",
+		temperature: 0.6,
+		max_tokens: 150,
+		top_p: 1,
+		frequency_penalty: 1,
+		presence_penalty: 1
+	});
 
-    console.log(gptResponse.data);
-})();
+	const paragraph = document.querySelector(".test");
+	paragraph.textContent = gptResponse.data.choices[0].text;
+	console.log(gptResponse.data.choices[0].text);
+};
+
+main();
