e24b0d64f86a5298053145ab60b4a5f0b432dcd6
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-05-14 12:56:42 -0400

    starting to work on style

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,21 @@ const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 const openai = new OpenAI(OPENAI_API_KEY);
 
 const main = async () => {
+	// const words: string[] = [];
+	// const word = document.querySelector(".madlib-form__input");
+	// const submit = document.querySelector(".madlib-form__submit-button");
+	// submit?.addEventListener("click", () => {
+	// 	words.push(word);
+	// });
+
 	const gptResponse = await openai.complete({
 		engine: "text-davinci-002",
-		prompt: "tell me a happy story",
+		prompt: "tell me a happy story about dogs",
 		temperature: 0.6,
-		max_tokens: 150,
-		top_p: 1,
-		frequency_penalty: 1,
-		presence_penalty: 1
+		maxTokens: 150,
+		topP: 1,
+		frequencyPenalty: 1,
+		presencePenalty: 1
 	});
 
 	const paragraph = document.querySelector(".test");
