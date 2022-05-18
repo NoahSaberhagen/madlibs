06e64e6bb0537b716697c90082c1982512ee108d
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-05-18 19:10:07 -0400

    beginning to add user input words feature

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,13 @@ const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 const openai = new OpenAI(OPENAI_API_KEY);
 
 const main = async () => {
-	// const words: string[] = [];
-	// const word = document.querySelector(".madlib-form__input");
-	// const submit = document.querySelector(".madlib-form__submit-button");
-	// submit?.addEventListener("click", () => {
-	// 	words.push(word);
-	// });
+	const storyField: Element = document.querySelector(".story-field");
+  storyField.setAttribute("style", "animation-name: none;");
+	storyField.textContent = "loading...";
 
 	const gptResponse = await openai.complete({
 		engine: "text-davinci-002",
-		prompt: "tell me a funny story using these words: oatmeal, triangular, sticky notes",
+		prompt: "tell me a funny story using these words: CSS, HTML, Javascript",
 		temperature: 0.6,
 		maxTokens: 150,
 		topP: 1,
@@ -21,12 +18,24 @@ const main = async () => {
 		presencePenalty: 1
 	});
 
-	const paragraph = document.querySelector(".story-field");
-	paragraph.textContent = gptResponse.data.choices[0].text;
-	paragraph.setAttribute("style", "animation-name: fade-in;");
-	console.log(gptResponse.data.choices[0].text);
+	storyField.textContent = gptResponse.data.choices[0].text;
+	storyField.setAttribute("style", "animation-name: fade-in;");
 };
 
 main();
 
+
+
+const submitButton = document.querySelector(".madlib-form__submit-button");
+submitButton?.addEventListener("click", () => {
+	const input: string = document.querySelector(".madlib-form__input").value;
+	const listOfWords: Element = document.querySelector(".list-of-words");
+
+	const newWord = document.createElement("li");
+	newWord.textContent = input;
+
+	listOfWords.appendChild(newWord);
+
+	main();
+});
 
