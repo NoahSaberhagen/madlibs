cc360f9fe9b44ca7572a143900eb3ea4d10b1cba
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-05-18 21:17:21 -0400

    api disconnected

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const openai = new OpenAI(OPENAI_API_KEY);
 
 
 const form = document.querySelector(".madlib-form");
-const goingToPrompt: string[] = ["HTML, CSS, Javascript"];
+let goingToPrompt: string[] = ["HTML, CSS, Javascript"];
 
 
 
@@ -36,7 +36,6 @@ form?.addEventListener("submit", (e) => {
 	const input: string = document.querySelector(".madlib-form__input").value;
 	const listOfInputs: Element = document.querySelector(".list-of-inputs");
 
-
 	const newWord = document.createElement("li");
 
 	newWord.textContent = input;
@@ -48,10 +47,22 @@ form?.addEventListener("submit", (e) => {
 		const wordForPrompt: string = wordsForPrompt[i].textContent;
 		goingToPrompt[i] = wordForPrompt;
 	}
+});
+
+
+const generateStoryButton = document.querySelector(".generate-story");
+generateStoryButton?.addEventListener("click", () => {
 	main();
 });
 
 
+const clearButton = document.querySelector(".clear-list-of-inputs");
+clearButton?.addEventListener("click", () => {
+	const listOfInputs: Element = document.querySelector(".list-of-inputs");
+	listOfInputs.textContent = undefined;
+	goingToPrompt = ["HTML, CSS, Javascript"];
+	main();
+})
 
 
 
