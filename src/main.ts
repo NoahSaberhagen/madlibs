38da091a2437acb1f7223d584050b17f839e0ed2
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-05-21 22:04:05 -0400

    done for the day

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,20 @@ const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 const openai = new OpenAI(OPENAI_API_KEY);
 
 //global variables
-const counter = document.querySelector(".counter") as HTMLParagraphElement;
+
+const counterDisplay = document.querySelector(".counter") as HTMLParagraphElement;
 const form = document.querySelector(".madlib-form");
 const inputField = document.querySelector(".madlib-form__input") as HTMLInputElement;
 const storyField = document.querySelector(".story-field") as HTMLParagraphElement;
-let storyWords: string[] = [];
 const wordList = document.querySelector(".list-of-story-words") as HTMLUListElement;
+
+let storyWords: string[] = [];
+const counter = storyWords.length;
 
 //justUXthings
 inputField.focus();
+storyField.style.color = "#6A9955";
+
 
 //mini functions
 const populateStoryWords = () => {
@@ -25,16 +30,16 @@ const populateStoryWords = () => {
 const removeStringDuplicates = (arr: string[]) => {
 	return arr.filter((item, index) => arr.indexOf(item) === index);
 };
-const updateCounter = () => {
-	counter.textContent = storyWords.length + "/5";
+const updateCounterDisplay = () => {
+	counterDisplay.textContent = storyWords.length + "/5";
 };
 
 //bigger functions
 //grabs response from api call using specified prompt and displays it in .story-field
 const main = async () => {
 	storyField.setAttribute("style", "animation-name: none;");
-	storyField.style.fontSize = "1.5rem";
-	storyField.textContent = "loading...";
+	storyField.style.color = "#6A9955";
+	storyField.textContent = "//loading...";
 	
 	const gptResponse = await openai.complete({
 		engine: "text-davinci-002",
@@ -48,22 +53,29 @@ const main = async () => {
 
 	const story: string = gptResponse.data.choices[0].text;
 	story.trimStart();
-	
+
+	storyField.style.color = "#D4D4D4";
+
 	//DOM manipulation makes me feel like a mad scientist
-	//*     /(HTML|CSS|JavaScript)/ig
 	const storyRegExp = new RegExp(`(${storyWords.join("|")})`, "ig");
 	
 	const storyHTML = story.replace(storyRegExp, "<span class=\"orange placeholder-class\">$&</span>");
 
 	storyField.style.fontSize = "2rem";
 	storyField.innerHTML = storyHTML;
-	// storyField.setAttribute("style", "animation-name: fade-in;");
+	//this is in case I change my mind and want to restyle the animations:
+	//storyField.setAttribute("style", "animation-name: fade-in;");
 };
 
-//submitting form updates prompt
+//submitting the form updates the prompt going to OpenAI
 form?.addEventListener("submit", (e) => {
 	//so the page doesn't refresh
 	e.preventDefault();
+
+	if(storyWords.length >= 5){
+		alert("you have reached maximum words");
+		return;
+	}
 
 	const input = document.querySelector(".madlib-form__input") as HTMLInputElement;
 	
@@ -75,11 +87,10 @@ form?.addEventListener("submit", (e) => {
 	
 	inputField.value = "";
 	inputField.focus();
-	
+
 	populateStoryWords();
 	removeStringDuplicates(storyWords);
-	updateCounter();
-	console.log(storyWords);
+	updateCounterDisplay();
 });
 
 //generate
@@ -96,10 +107,12 @@ clear?.addEventListener("click", () => {
 	storyWords = [];
 	
 	wordList.innerHTML = "";
-	
-	storyField.textContent = "enter words ---->";
 
-	updateCounter();
+	storyField.style.color = "#6A9955";
+	storyField.innerHTML = `//add some words using the input field over there --><br>
+	//click the generate() button when you're ready to load`;
+
+	updateCounterDisplay();
 	
 	inputField.focus();
 });
