e1f120261b136139570546621508b02ead89e105
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-05-25 13:38:09 -0400

    TODO rename main(), add style/mood, improve UI

--- src/main.ts
+++ src/main.ts
@@ -4,34 +4,36 @@ const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 
 const openai = new OpenAI(OPENAI_API_KEY);
 
-//global variables
+//global variables-----------------------------------------------------------------------------------------
 const counterDisplay = document.querySelector(".counter") as HTMLParagraphElement;
 const form = document.querySelector(".madlib-form");
 const inputField = document.querySelector(".madlib-form__input") as HTMLInputElement;
 const storyField = document.querySelector(".story-field") as HTMLParagraphElement;
 const storyWordsField = document.querySelector(".list-of-story-words") as HTMLUListElement;
-
+const storyGenre: string = 
+	document.querySelector(".genre-select") === null ? "" : document.querySelector(".genre-select").value
 let storyWords: string[] = [];
 
-//behind-the-scenes functions
+// loads UX-----------------------------------------------------------------------------------------------
+inputField.focus();
+storyField.style.color = "#6A9955";
+
+// behind-the-scenes functions----------------------------------------------------------------------------
 const addWordToStoryWords = () => {
-	const words = document.getElementsByClassName("story-word");
-	for(let i = 0; i < words.length; i++){
-		storyWords[i] = words[i].textContent as string;
+	const storyWordElements = document.getElementsByClassName("story-word");
+	for(let i = 0; i < storyWordElements.length; i++){
+		storyWords[i] = storyWordElements[i].textContent as string;
 	}
 };
 const removeDuplicates = (arr: string[]) => {
-	return arr.filter((item, index) => arr.indexOf(item) === index);
+	return Array.from(new Set(arr));
 };
 const updateCounterDisplay = () => {
 	counterDisplay.textContent = storyWords.length + "/5";
 };
 
-//loads UX
-inputField.focus();
-storyField.style.color = "#6A9955";
-
-//user-facing functions
+// user-facing functions--------------------------------------------------------------------------------
+// TODO rename main
 const main = async () => {
 	storyField.setAttribute("style", "animation-name: none;");
 	storyField.style.color = "#6A9955";
@@ -39,7 +41,7 @@ const main = async () => {
 	
 	const gptResponse = await openai.complete({
 		engine: "text-davinci-002",
-		prompt: "tell me a funny story including each of the following words:" + storyWords.join(" "),
+		prompt: "tell me a" + storyGenre + " story using all of these words:" + storyWords.join(" "),
 		temperature: 0.6,
 		maxTokens: 150,
 		topP: 1,
@@ -50,12 +52,23 @@ const main = async () => {
 	const story: string = gptResponse.data.choices[0].text;
 	story.trimStart();
 
+	// UI
 	storyField.style.color = "#D4D4D4";
 
-	//DOM manipulation makes me feel like a mad scientist
-	const storyRegExp = new RegExp(`(${storyWords.join("|")})`, "ig");
-	
-	const storyHTML = story.replace(storyRegExp, "<span class=\"orange placeholder-class\">$&</span>");
+	const storyWordsRegExp = storyWords.map((storyWord) => {
+		let newStoryWord = storyWord;
+		if(storyWord[storyWord.length - 1].toLowerCase() === "s"){
+			newStoryWord = storyWord.slice(0, -1);
+		}
+		return `( ${newStoryWord}( |'s|s))\\W*`;
+	});
+	console.log(storyWords);
+
+	const storyRegExp = new RegExp(`(${storyWordsRegExp.join("|")})`, "ig");
+
+	console.log(storyRegExp);
+
+	const storyHTML = story.replace(storyRegExp, "<span class=\"orange fade-in-slowly\">$&</span>");
 
 	storyField.style.fontSize = "2rem";
 	storyField.innerHTML = storyHTML;
@@ -116,18 +129,19 @@ form?.addEventListener("submit", (e) => {
 	inputField.focus();
 
 	addWordToStoryWords();
-	removeDuplicates(storyWords);
+
 	updateCounterDisplay();
+
+	console.log(storyWords);
 });
 
-//generate
 const generate = document.querySelector(".madlib-form__generate");
 generate?.addEventListener("click", () => {
+	storyWords = removeDuplicates(storyWords);
 	main();
 	inputField.focus();
 });
 
-//clear
 const clear = document.querySelector(".madlib-form__clear");
 clear?.addEventListener("click", () => {
 	storyWords = [];
